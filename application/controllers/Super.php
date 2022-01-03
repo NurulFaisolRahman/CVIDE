0751ca56984b85fb591c2e309e0ec8bac9fe204e
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2022-01-03 17:00:47 +0700

    Revisi Indeks Komoditas Terpilih

--- application/controllers/Super.php
+++ application/controllers/Super.php
@@ -945,9 +945,11 @@ class Super extends CI_Controller {
       $Harga = explode("|",$key['Harga']);
       $Indeks = 0;
       for ($i=0; $i < count($Nilai); $i++) { 
+        if (in_array($i,$IndeksKomoditasTerpilih)) {
           $TotalPengeluaran += (Int)$Nilai[$i];
           $KomoditasTerpilih[$Indeks] += (Int)$Harga[$i];
           $Indeks += 1;
+        }
         // if (in_array($i,array(0,2,3,4,5,6,7,8,9,10,11,12,13,14,16,19,20,21,23,24,25,36,37,38,39,40,41,62,63,64,66,68,69,71,72,76,79,84,85,86,87,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,113,114,115,118,121,141))) {
         //   $TotalPengeluaran += (Int)$Nilai[$i]*152;
         //   $KomoditasTerpilih[$Indeks] += (Int)$Harga[$i];
