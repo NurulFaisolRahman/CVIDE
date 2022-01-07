4d9a1f44be42999ff68a9f3f192a26fa3762f59c
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2022-01-07 16:02:05 +0700

    Revisi Data Desa Belum Tersedia

--- application/views/Super/Pendidikan.php
+++ application/views/Super/Pendidikan.php
@@ -111,17 +111,21 @@
           })    
         })
         $("#TampilkanData").click(function() {
-          var Data =  { KodeDesa: $("#Desa").val(),
-                        KodeKecamatan: $("#Kecamatan").val(),
-                        JenisData: $("#JenisData").val() }
-          $.post(BaseURL+"Super/Session", Data).done(function(Respon) {
-            if (Respon == '1') {
-              window.location = BaseURL + "Super/Pendidikan"
-            }
-            else {
-              alert(Respon)
-            }
-          })                    
+          if ($("#JenisData").val() == 'Desa') {
+            alert('Data Pendidikan Desa Belum Tersedia')
+          } else {
+            var Data =  { KodeDesa: $("#Desa").val(),
+                          KodeKecamatan: $("#Kecamatan").val(),
+                          JenisData: $("#JenisData").val() }
+            $.post(BaseURL+"Super/Session", Data).done(function(Respon) {
+              if (Respon == '1') {
+                window.location = BaseURL + "Super/Pendidikan"
+              }
+              else {
+                alert(Respon)
+              }
+            })                    
+          }
         })
         google.charts.load("current", {packages:["corechart"]});
         google.charts.setOnLoadCallback(drawChart);
