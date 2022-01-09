8a30ef367e4635f0031c52c560eaec3437adc3a1
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2022-01-09 18:34:24 +0700

    Fix Rekap 4.0

--- application/controllers/Super.php
+++ application/controllers/Super.php
@@ -1078,8 +1078,8 @@ class Super extends CI_Controller {
       $Data['IndeksPengeluaran'] = $IndeksPengeluaran[(int)substr($Data['KodeKecamatan'],-2)-1];
     }
     if ($this->session->userdata('JenisData') == 'Kabupaten') {
-      $Data['PerKapita'] = 12910923; 
-      $Data['PerKapitaKonstan'] = 12215842; 
+      $Data['PerKapita'] = 10759;
+      $Data['PerKapitaKonstan'] = 10180;
       $Data['IndeksPengeluaran'] = 0.74;
     }
     $this->load->view('Super/Header',$Data);
