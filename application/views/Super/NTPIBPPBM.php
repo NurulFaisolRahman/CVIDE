9270765cad50efe11b81caedb677a4ad3f862b95
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2022-08-08 00:23:23 +0700

    Revisi NTPIBPPBM 2 Digit Koma

--- application/views/Super/NTPIBPPBM.php
+++ application/views/Super/NTPIBPPBM.php
@@ -2,7 +2,7 @@
             <div class="row">
               <div class="col-lg-12">
                 <div class="row mt-1">
-                  <div class="col-lg-6 col-sm-12">
+                  <div class="col-lg-8 col-sm-12">
                     <div class="table-responsive">
                       <table class="table table-sm table-bordered table-striped">
                         <thead class="bg-danger">
@@ -20,60 +20,60 @@
                         <tbody style="font-size: 12px;" class="bg-primary">
                           <tr class="text-light align-middle">
                             <td class="align-middle"><b>Bibit</b></td>
-                            <td class="align-middle"><b><?=$IBibit[0]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[1]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[2]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[3]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[4]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[5]?></b></td>
-                            <td class="align-middle"><b><?=$IBibit[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBibit[6],2)?></b></td>
                           </tr>
                           <tr class="text-light align-middle">
                             <td class="align-middle"><b>Pupuk & Obat-Obatan</b></td>
-                            <td class="align-middle"><b><?=$IPupuk[0]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[1]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[2]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[3]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[4]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[5]?></b></td>
-                            <td class="align-middle"><b><?=$IPupuk[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IPupuk[6],2)?></b></td>
                           </tr><tr class="text-light align-middle">
                             <td class="align-middle"><b>Transportasi</b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[0]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[1]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[2]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[3]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[4]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[5]?></b></td>
-                            <td class="align-middle"><b><?=$ITransportasi[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ITransportasi[6],2)?></b></td>
                           </tr><tr class="text-light align-middle">
                             <td class="align-middle"><b>Sewa & Pengeluaran Lain</b></td>
-                            <td class="align-middle"><b><?=$ISewa[0]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[1]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[2]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[3]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[4]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[5]?></b></td>
-                            <td class="align-middle"><b><?=$ISewa[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($ISewa[6],2)?></b></td>
                           </tr>
                           </tr><tr class="text-light align-middle">
                             <td class="align-middle"><b>Barang Modal</b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[0]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[1]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[2]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[3]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[4]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[5]?></b></td>
-                            <td class="align-middle"><b><?=$IBarangModal[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IBarangModal[6],2)?></b></td>
                           </tr><tr class="text-light align-middle">
                             <td class="align-middle"><b>Upah Buruh</b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[0]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[1]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[2]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[3]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[4]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[5]?></b></td>
-                            <td class="align-middle"><b><?=$IUpahBuruh[6]?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[0],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[1],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[2],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[3],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[4],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[5],2)?></b></td>
+                            <td class="align-middle"><b><?=number_format($IUpahBuruh[6],2)?></b></td>
                           </tr>
                         </tbody>
                       </table>
