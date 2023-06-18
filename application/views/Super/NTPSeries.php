1c79c1dfaad25690fdc921f570bc10225da70c18
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2023-06-18 13:56:53 +0700

    NTP Juni 2023

--- application/views/Super/NTPSeries.php
+++ application/views/Super/NTPSeries.php
@@ -33,9 +33,9 @@
                           <th class="align-middle">Maret</th>
                           <th class="align-middle">April</th>
                           <th class="align-middle">Mei</th>
-                        <?php if ($Tahun == 2022) { ?>
                           <th class="align-middle">Juni</th>
                           <th class="align-middle bg-primary">Semester 1</th>
+                        <?php if ($Tahun == 2022) { ?>
                           <th class="align-middle">Juli</th>
                           <th class="align-middle">Agustus</th>
                           <th class="align-middle">September</th>
@@ -113,9 +113,9 @@
                           <td class="align-middle"><b><?=number_format($IT[2][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IT[3][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IT[4][$i],2)?></b></td>
-                          <!-- <td class="align-middle"><b><?=number_format($IT[5][$i],2)?></b></td>
+                          <td class="align-middle"><b><?=number_format($IT[5][$i],2)?></b></td>
                           <td class="align-middle bg-danger text-center"><b><?=number_format(($IT[0][$i]+$IT[1][$i]+$IT[2][$i]+$IT[3][$i]+$IT[4][$i]+$IT[5][$i])/6,2)?></b></td>
-                          <td class="align-middle"><b><?=number_format($IT[6][$i],2)?></b></td>
+                          <!-- <td class="align-middle"><b><?=number_format($IT[6][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IT[7][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IT[8][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IT[9][$i],2)?></b></td>
@@ -131,9 +131,9 @@
                           <td class="align-middle"><b><?=number_format($IB[2][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IB[3][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IB[4][$i],2)?></b></td>
-                          <!-- <td class="align-middle"><b><?=number_format($IB[5][$i],2)?></b></td>
+                          <td class="align-middle"><b><?=number_format($IB[5][$i],2)?></b></td>
                           <td class="align-middle bg-danger text-center"><b><?=number_format(($IB[0][$i]+$IB[1][$i]+$IB[2][$i]+$IB[3][$i]+$IB[4][$i]+$IB[5][$i])/6,2)?></b></td>
-                          <td class="align-middle"><b><?=number_format($IB[6][$i],2)?></b></td>
+                          <!-- <td class="align-middle"><b><?=number_format($IB[6][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IB[7][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IB[8][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($IB[9][$i],2)?></b></td>
@@ -149,9 +149,9 @@
                           <td class="align-middle"><b><?=number_format($NTP[2][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($NTP[3][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($NTP[4][$i],2)?></b></td>
-                          <!-- <td class="align-middle"><b><?=number_format($NTP[5][$i],2)?></b></td>
+                          <td class="align-middle"><b><?=number_format($NTP[5][$i],2)?></b></td>
                           <td class="align-middle bg-danger text-center"><b><?=number_format(($NTP[0][$i]+$NTP[1][$i]+$NTP[2][$i]+$NTP[3][$i]+$NTP[4][$i]+$NTP[5][$i])/6,2)?></b></td>
-                          <td class="align-middle"><b><?=number_format($NTP[6][$i],2)?></b></td>
+                          <!-- <td class="align-middle"><b><?=number_format($NTP[6][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($NTP[7][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($NTP[8][$i],2)?></b></td>
                           <td class="align-middle"><b><?=number_format($NTP[9][$i],2)?></b></td>
