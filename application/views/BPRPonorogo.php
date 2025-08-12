333d67611d3fef4148bb46edf5e28bbe41cf9dab
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2025-08-12 20:19:29 +0700

    Microsite BPRPonorogo

---
+++ application/views/BPRPonorogo.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>BPR Kabupaten Ponorogo</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+  </head>
+  <body style="background-color: #d3e2e3;">
+    
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 text-center">
+          <img style="position: relative;" class="img-rounded" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Kantor_Bupati_Ponorogo.jpg/1200px-Kantor_Bupati_Ponorogo.jpg" alt="Responsive image" width="400">
+          <img style="position: absolute;transform: translate(-50%,-50%);margin-top: 210px;margin-left: -200px;" class="img-rounded" src="https://pemajuankebudayaan.id/wp-content/uploads/2022/05/Lambang_Kabupaten_Ponorogo_1969-1536x1512.png" alt="Responsive image" width="150">
+        </div>  
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div style="margin-top: 45px;" class="row">
+        <div class="col-12 text-center">
+          <h3><b>Kabupaten Ponorogo</b></h3>
+        </div>  
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div style="margin-top: 5px;" class="row">
+        <div class="col-12 text-center">
+          <div style="width: 400px;margin-bottom: 55px;" class="container">
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab">
+                  <h4 style="text-align: left;" class="panel-title">
+                    <a style="text-decoration: none;" role="button" data-toggle="collapse" data-parent="#accordion" href="#Dinsos" aria-expanded="true" aria-controls="Dinsos">
+                      <b><span class="glyphicon glyphicon-list" aria-hidden="true"></span> BPR Ponorogo</b>
+                    </a>
+                  </h4>
+                </div>
+                <div id="Dinsos" class="panel-collapse collapse" role="tabpanel">
+                  <div class="panel-body">
+                    <div style="margin-bottom: 0px;" class="panel-group" id="accordionDinsos" role="tablist" aria-multiselectable="true">
+                      <div class="panel panel-default">
+                        <div class="panel-heading" role="tab">
+                          <h4 style="text-align: left;" class="panel-title">
+                            <a style="text-decoration: none;" role="button" data-toggle="collapse" data-parent="#accordionDinsos" href="#DinsosPERMENDAGRI" aria-expanded="true" aria-controls="DinsosPERMENDAGRI">
+                              <b><span class="glyphicon glyphicon-list" aria-hidden="true"></span> Kebutuhan Data</b>
+                            </a>
+                          </h4>
+                        </div>
+                        <div id="DinsosPERMENDAGRI" class="panel-collapse collapse" role="tabpanel">
+                          <div style="padding: 10px;font-size: 12pt;font-family: Cambria;letter-spacing: 1px;" class="panel-body">
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/spreadsheets/d/1naDerHzGIgrxhwhbH73BXni85j0-TYo12gVrtywsH6E/edit?gid=0#gid=0')" role="button"><b>Kertas Kerja</b></a>
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/document/d/1RJV6GLnPciqQtzZxL2BlWHMh0VM_tMcgMdizbxkw27s/edit?tab=t.0 ')" role="button"><b>BAB I</b></a>
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/document/d/1oh_v2jN2cuKy7jIUCyCeHR_Xq_F0nTNDR8ppQ8aZMCQ/edit?tab=t.0#heading=h.su4oczdpgxi  ')" role="button"><b>BAB II</b></a>
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/document/d/1gB7vK3KD31EmzsTLAAyzIs-sPZQ2u_-6qtaxAqW5pu8/edit?tab=t.0  ')" role="button"><b>BAB III</b></a>
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/document/d/1mreRMBSFJKGHLuYQ5Vx4WyUdSv_SfpFvhrwFXs8Up9w/edit?tab=t.0 ')" role="button"><b>BAB IV</b></a>
+                            <a class="btn btn-primary btn-block" onclick="window.open('https://docs.google.com/document/d/1PpnU6pAp5sGdKsgQMTWkHrE8KIy1YH8yjjhNu1fqwVM/edit?tab=t.0 ')" role="button"><b>BAB V</b></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  </body>
+</html>
