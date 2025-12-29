f49e413e79a6f009e469270137aa3f53bd0067f9
kaka12 <123827389+12kaka@users.noreply.github.com>
2025-12-29 15:17:09 +0700

    Revisi request dila2

--- application/views/Admin/Dashboard.php
+++ application/views/Admin/Dashboard.php
@@ -34,6 +34,21 @@
 		<script>
 			$(document).ready(function(){
 				var BaseURL = '<?=base_url()?>'  
+				
+				// Toggle visibility password
+				$('.password-toggle').click(function() {
+					var passwordField = $('#Password');
+					var icon = $(this).find('i');
+					
+					if (passwordField.attr('type') === 'password') {
+						passwordField.attr('type', 'text');
+						icon.removeClass('fa-eye').addClass('fa-eye-slash');
+					} else {
+						passwordField.attr('type', 'password');
+						icon.removeClass('fa-eye-slash').addClass('fa-eye');
+					}
+				});
+				
 				$("#GantiPassword").click(function() {
 					if ($("#Password").val() === "") {
 						alert('Password Tidak Boleh Kosong')
